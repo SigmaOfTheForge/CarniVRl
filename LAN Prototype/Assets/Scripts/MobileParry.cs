ef7e4121bb40216c617021fd8f2ab06a65cf202e
IncompetentTrout <tlam2020@elam.co.uk>
2025-02-26 11:04:46 +0000

    improved parry system so no singleton

--- LAN Prototype/Assets/Scripts/MobileParry.cs
+++ LAN Prototype/Assets/Scripts/MobileParry.cs
@@ -5,41 +5,84 @@ using Unity.Netcode;
 
 public class MobileParry : NetworkBehaviour
 {
+    private IEnumerator parryAttackWindow;
+    private bool isParryEnabled = false;
+    private bool isParryWindowActive = false;
+    [SerializeField] private float parryWindow;
+    
     [SerializeField] Transform vrPlayer;
-    bool isParrying;
+    bool isParryButtonPressed;
 
     private void Start()
     {
         vrPlayer = GameObject.FindGameObjectWithTag("VR_Player_Manager").GetComponent<VRPlayerManager>().GetCurrentPlayerTransform();
     }
 
+    private void StartParryWindow()
+    {
+        if (parryAttackWindow != null)
+        {
+            StopCoroutine(parryAttackWindow);
+        }
+        parryAttackWindow = ParryWindowCoroutine();
+        StartCoroutine(parryAttackWindow);
+    }
+
+    private IEnumerator ParryWindowCoroutine()
+    {
+        isParryEnabled = true;
+        isParryWindowActive = true;
+        yield return new WaitForSeconds(parryWindow);
+        ResetParryWindow();
+    }
+
+    private void ResetParryWindow()
+    {
+        if (parryAttackWindow != null)
+        {
+            StopCoroutine(parryAttackWindow);
+            parryAttackWindow = null;
+        }
+        isParryEnabled = false;
+        isParryWindowActive = false;
+    }
+
+    private bool IsParryWindowActive()
+    {
+        return isParryWindowActive;
+    }
+
+    private void HandleAttack(bool canParry, bool canBlock, GameObject ball, Rigidbody brb, Transform cannon)
+    {
+        if (isParryEnabled && canParry)
+        {
+            Debug.Log("Parried");
+            ball.transform.LookAt(cannon);
+            brb.useGravity = false;
+            brb.velocity = ball.transform.forward * 50; //make force depend on distance as well //naw that was a bad idea
+        }
+        else if (canBlock)
+        {
+            Debug.Log("Block Performed");
+        }
+        else
+        {
+            ball.SetActive(false);
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown("e")) //get mobile button
         {
-            HandleBlockPressed();
-            isParrying = true;
+            StartParryWindow();
+            isParryButtonPressed = true;
         }
         if (Input.GetKeyUp("e")) //get mobile button
         {
-            HandleBlockReleased();
-            isParrying = false;
+            ResetParryWindow();
+            isParryButtonPressed = false;
         }
-    }
-
-    private void HandleBlockPressed()
-    {
-        ParryManager.SharedInstance.StartParryWindow();
-    }
-
-    private void HandleBlockReleased()
-    {
-        ParryManager.SharedInstance.ResetParryWindow();
-    }
-
-    private void OnAttackRecieved(bool canParry, bool canBlock, GameObject ball, Rigidbody brb, Transform cannon)
-    {
-        ParryManager.SharedInstance.HandleAttack(canParry, canBlock, ball, brb, cannon);
     }
     
     void OnCollisionEnter(Collision other)
@@ -48,13 +91,13 @@ public class MobileParry : NetworkBehaviour
         {
             Rigidbody brb = other.gameObject.GetComponent<Rigidbody>();
             
-            if (isParrying)
+            if (isParryButtonPressed)
             {
-                OnAttackRecieved(true, true, other.gameObject, brb, vrPlayer);
+                HandleAttack(true, true, other.gameObject, brb, vrPlayer);
             }
             else
             {
-                OnAttackRecieved(false, false, other.gameObject, brb, vrPlayer);
+                HandleAttack(false, false, other.gameObject, brb, vrPlayer);
             }
         }
     }
